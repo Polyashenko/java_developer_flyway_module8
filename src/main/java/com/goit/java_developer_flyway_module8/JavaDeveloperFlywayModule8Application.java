7584eefbe5b66e1dde5c47c084783a14ed6d6454
Dmitry <Polyashenko1@gmail.com>
2023-09-10 19:44:12 +0300

    first commit

---
+++ src/main/java/com/goit/java_developer_flyway_module8/JavaDeveloperFlywayModule8Application.java
@@ -0,0 +1,13 @@
+package com.goit.java_developer_flyway_module8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDeveloperFlywayModule8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDeveloperFlywayModule8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/goit/java_developer_flyway_module8/JavaDeveloperFlywayModule8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goit.java_developer_flyway_module8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDeveloperFlywayModule8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
